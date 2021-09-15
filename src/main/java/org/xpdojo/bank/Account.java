44f2d4192d5c6730b7b5a7962fc87275c809de4b
nageswar-gorle <90594085+nageswar-gorle@users.noreply.github.com>
2021-09-15 14:16:51 +0200

    Update Account.java

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,16 @@
 package org.xpdojo.bank;
 
 public class Account {
+  
+  public int balance;
+  public void deposit(int amount)
+  {
+    balance=+amount:
+  }
+  
+  public void withdrawal(int amount)
+  {
+    balance-+amount;
+  }
+  
 }
